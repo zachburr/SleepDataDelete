3dce4701cdce06e12bc4d245955278ccd1a41114
Zachburr <zburr@my.wctc.edu>
2025-09-02 13:35:11 -0500

    [1.02] create user options

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+//ask for input
+Console.WriteLine("Enter 1 to create data file");
+Console.WriteLine("Enter 2 ro parse data file");
+Console.WriteLine("Enter anything else to quit");
+
+string? resp = Console.ReadLine();
+
+if (resp == "1")
+{
+
+}
+else if (resp == "2")
+{
+
+}
