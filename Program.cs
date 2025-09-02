ad0cf64b8a8bb4e9f0570679f5bd45123f789ca6
Zachburr <zburr@my.wctc.edu>
2025-09-02 13:55:39 -0500

    [1.04]weeks

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Data;
+
 Console.WriteLine("Hello, World!");
 //ask for input
 Console.WriteLine("Enter 1 to create data file");
@@ -9,9 +11,18 @@ string? resp = Console.ReadLine();
 
 if (resp == "1")
 {
+    //create data file
+    //ask question
     Console.WriteLine("How many weeks of data is needed?");
     //input the response (convert to int)
     int weeks = Convert.ToInt32(Console.ReadLine());
+    //determine start and end date
+    DateTime today = DateTime.Now;
+    //we want full weeks sunday-saturday
+    DateTime dataEndDate = today.AddDays(-(int)today.DayOfWeek);
+    //subtract # of weeks from endDate to get startDate
+    DateTime dataDate = dataEndDate.AddDays(-(weeks * 7));
+    Console.WriteLine(dataDate);
 }
 else if (resp == "2")
 {
